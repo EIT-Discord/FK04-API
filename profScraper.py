32f3a505687e27568ce4ca08703a4f37f39e7271
YannicBreiting <63100391+YannicBreiting@users.noreply.github.com>
2021-09-27 14:48:57 +0200

    Update profScraper.py

--- profScraper.py
+++ profScraper.py
@@ -22,14 +22,37 @@ for entry in table:
 for url in htmls:
     file = requests.get(url)
     soup = BeautifulSoup(file.text, 'html.parser')
+    person_detail = soup.find('h2', class_='normal')
     contact = soup.find('div', class_='contact-person-detail-contact')
     faculty = soup.find('div', class_='contact-person-detail-faculty')
     image = soup.find('img', class_='portrait')
 
+    try:
+        moodle_courses = soup.find('a', href=re.compile(r'https://moodle.hm.edu/course')).attrs['href']
+    except AttributeError:
+        moodle_courses = ''
+
+    try:
+        person_description = soup.find(class_='list-contact-person-detail')
+        description = person_description.text
+    except AttributeError:
+        description = ''
+
+    name = person_detail.text
     phone, fax = (re.sub(r'[^0-9 -]', '', field) for field in contact.text.strip().split('\n')[:2])
     image_url = image.attrs['src']
 
-    new_prof = Professor(phone=phone, fax=fax, image_url=image_url)
+
+    room = ''
+    address = ''
+    for string in faculty.text.strip().split('\n'):
+        if 'Raum:' in string:
+            room = string[string.find(' '):]
+        if 'Adresse' in string:
+            address = string[string.find(' '):]
+
+    new_prof = Professor(name=name, phone=phone, fax=fax, address=address, room=room, moodleCourses=moodle_courses,
+                         description=description, image_url=image_url)
     db.session.add(new_prof)
 
 db.session.commit()
