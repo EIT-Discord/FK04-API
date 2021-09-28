48f26dbe2e37aeddaa3d69d5ea0e017f175198a6
Martin Kistler <martin.kistler@live.de>
2021-09-28 13:39:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,38 @@
-from flask import Flask
+import uuid
+
+from flask import Flask, request
 from flask_restful import Api, Resource, fields, marshal
 from flask_sqlalchemy import SQLAlchemy
 from pathlib import Path
 
+
 PROJECT_ROOT = Path(__file__).parent
-print(PROJECT_ROOT)
+
 
 app = Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{PROJECT_ROOT}/data.sqlite'
 db = SQLAlchemy(app)
+
+
+def need_authorization(function):
+    def authorization(*args, **kwargs):
+        auth = request.headers.get('Authorization')
+
+        if auth is None:
+            return {'message': 'UNAUTHORIZED'}, 401
+
+        if not auth.startswith('Bearer '):
+            return {'message': 'Invalid authentication scheme'}, 401
+
+        token = auth.lstrip('Bearer ')
+
+        if not ActiveToken.query.filter_by(token=token).first():
+            return {'message': 'Invalid token provided'}, 401
+
+        return function(*args, **kwargs)
+
+    return authorization
 
 
 class Professor(db.Model):
@@ -28,6 +51,34 @@ class Professor(db.Model):
         return self.name
 
 
+class APIUser(db.Model):
+    userame = db.Column(db.String(100), primary_key=True)
+    password = db.Column(db.String(100))
+
+
+class ActiveToken(db.Model):
+    token = db.Column(db.String(100), primary_key=True)
+    username = db.Column(db.String(100))
+
+
+class LoginAPI(Resource):
+    def post(self):
+        username = request.json.get('username')
+        password = request.json.get('password')
+
+        if username is None or password is None:
+            return {'message': 'No username or password provided with request.'}, 401
+
+        if not APIUser.query.filter_by(username=username, password=password).first():
+            return {'message': 'Incorrect username or password.'}, 401
+
+        token = str(uuid.uuid4())
+        db.session.add(ActiveToken(token=token, username=username))
+        db.session.commit()
+
+        return {'token': token}
+
+
 class ProfessorsAPI(Resource):
     fields = {
         'id': fields.Integer,
@@ -43,7 +94,13 @@ class ProfessorsAPI(Resource):
 
     }
 
+    @need_authorization
     def get(self):
+        name = request.args.get('name')
+
+        if name is not None:
+            return {'professors': marshal(Professor.query.filter(Professor.name.contains(name)).all(), self.fields)}
+
         return {'professors': marshal(Professor.query.all(), self.fields)}
 
 
@@ -61,6 +118,7 @@ class ProfessorAPI(Resource):
         'uri': fields.Url('professor', absolute=True)
     }
 
+    @need_authorization
     def get(self, id):
         return {'professors': marshal(Professor.query.filter_by(id=id).first(), self.fields)}
 
@@ -68,6 +126,8 @@ class ProfessorAPI(Resource):
 # Endpoint configuration
 api.add_resource(ProfessorsAPI, '/api/professors', endpoint='professors')
 api.add_resource(ProfessorAPI, '/api/professors/<int:id>', endpoint='professor')
+api.add_resource(ProfessorAPI, '/api/login', endpoint='login')
+
 
 if __name__ == '__main__':
     db.create_all()
